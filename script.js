e11135a635c34f59e0f0464897d94158cf2cbc20
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2024-11-08 00:00:21 +0100

    Buttons OK

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const openBtn=document.querySelector(".sidebar-btn");
 const closeBtn=document.querySelector(".close-btn")
 const menuWrapper= document.querySelector(".menu-wrapper");
-const headerNav=document.querySelector(".menu-list-mobile");
+
 
 openBtn.addEventListener("click",()=> {
     menuWrapper.classList.toggle("visible");
+    openBtn.classList.add("hidden");/*hamb menu hidden*/
     console.log("opened");
 });
 
 closeBtn.addEventListener("click",()=>{
 menuWrapper.classList.remove("visible");
+openBtn.classList.remove("hidden");
 console.log("closed");
 });
